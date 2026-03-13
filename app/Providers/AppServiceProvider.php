37c4ee44304cf4610b484b713ea1a72934ff8c58
Nikola Papratovic <nikola.papratovic@gmail.com>
2026-03-13 13:22:50 +0100

    added some defualts

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,8 +5,10 @@ namespace App\Providers;
 use Carbon\CarbonImmutable;
 use Illuminate\Support\Facades\Date;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Validation\Rules\Password;
+use Illuminate\Database\Eloquent\Model;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -24,6 +26,9 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         $this->configureDefaults();
+
+        // zend.exception_ignore_args: This PHP configuration directive controls whether arguments to exceptions are included in the stack trace. Setting it to 0 ensures that arguments are included, which can be helpful for debugging
+        ini_set('zend.exception_ignore_args', 0);
     }
 
     /**
@@ -36,6 +41,11 @@ class AppServiceProvider extends ServiceProvider
         DB::prohibitDestructiveCommands(
             app()->isProduction(),
         );
+        Schema::defaultStringLength(191);
+
+        if (!app()->isProduction()) {
+            Model::preventLazyLoading();
+        }
 
         Password::defaults(fn (): ?Password => app()->isProduction()
             ? Password::min(12)
